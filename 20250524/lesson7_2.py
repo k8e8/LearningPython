27392cd747106777f79f8d3b36491d643b3d9f07
k8e8 <lai20191012@gmail.com>
2025-05-24 11:59:36 +0800

    課堂練習

--- 20250524/lesson7_2.py
+++ 20250524/lesson7_2.py
@@ -1,19 +1,4 @@
-def caculate_bmi(height:int,weight:int)->float:
-    return weight / (height / 100) ** 2
-
-def get_state(bmi:float)->str:
-    if bmi <18.5:
-        return("體重過輕")
-    elif bmi < 24:
-        return("體重正常")
-    elif bmi < 27:
-        return("過重")
-    elif bmi < 30:
-        return("輕度肥胖")
-    elif bmi < 35:
-        return("中度肥胖")
-    else :
-        return("重度肥胖")
+import tools
 
 def main():
     height:int = int(input("請輸入身高(cm):"))
