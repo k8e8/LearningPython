a89159e02403dd8c03ffda2802674b690d53b6c3
k8e8 <lai20191012@gmail.com>
2025-05-24 12:02:13 +0800

    課堂練習

--- 20250524/lesson7_2.py
+++ 20250524/lesson7_2.py
@@ -4,9 +4,9 @@ def main():
     height:int = int(input("請輸入身高(cm):"))
     weight:int = int(input("請輸入體重(kg):"))
 
-    bmi = caculate_bmi(height, weight)
+    bmi = tools.caculate_bmi(height, weight)
     print(bmi)
-    print(get_state(bmi))
+    print(tools.get_state(bmi))
 
 if __name__ == '__main__':
     main()
